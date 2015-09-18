d2e6f22f4f1afe352061e1294a56e9734c433eb5
Kyle Winckler <kyle.winckler@gmail.com>
2015-09-17 20:14:03 -0700

    Add lucas function

--- hw/hw07/series.py
+++ hw/hw07/series.py
@@ -21,3 +21,29 @@ def fibonacci(n):
         return 8
     else:
         return 13
+
+
+def lucas(n):
+    """
+    Returns the nth numbe from the Lucas sequence.
+
+    Example: lucas(0) == 2
+             lucas(1) == 1
+             lucas(2) == 3
+    """
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 3
+    elif n == 3:
+        return 4
+    elif n == 4:
+        return 7
+    elif n == 5:
+        return 11
+    elif n == 6:
+        return 18
+    else:
+        return 29
