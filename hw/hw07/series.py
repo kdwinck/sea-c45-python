8a3aadf4458050f08502f58f80e3b1b91715f19e
Kyle Winckler <kyle.winckler@gmail.com>
2015-09-21 20:12:47 -0700

    corrected some syntax

--- hw/hw07/series.py
+++ hw/hw07/series.py
@@ -6,9 +6,10 @@ def fibonacci(n):
     Example: fibonacci(0) == 0
              fibonacci(1) == 1
     """
-    if n == 0:
+
+    if (n == 0):
         return 0
-    elif n == 1:
+    elif (n == 1):
         return 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
@@ -20,11 +21,13 @@ def lucas(n):
 
     Example: lucas(0) == 2
              lucas(1) == 1
-             lucas(2) == 3
+             ...
+             lucas(n) == lucas(n - 1) + lucas(n - 2)
     """
-    if n == 0:
+
+    if (n == 0):
         return 2
-    elif n == 1:
+    elif (n == 1):
         return 1
     else:
         return lucas(n - 1) + lucas(n - 2)
@@ -37,10 +40,13 @@ def sum_series(n, x=0, y=1):
 
     Example: sum_series(0, 2, 3) == 2
              sum_series(5, 0, 1) == 5
+             ...
+             sum_series(n) == sum_series(n - 1) + sum_series(n - 2)
     """
-    if n == 0:
+
+    if (n == 0):
         return x
-    elif n == 1:
+    elif (n == 1):
         return y
     else:
         return sum_series(n - 1, x, y) + sum_series(n - 2, x, y)
