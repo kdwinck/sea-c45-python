76924ca352c7e1a4cdbc909e059f4c5ca3f63e0e
Kyle Winckler <kyle.winckler@gmail.com>
2015-09-24 22:04:37 -0700

    Create donor list and home menu introduction

---
+++ hw/hw11/mailroom.py
@@ -0,0 +1,20 @@
+donor_list = [
+    ["Neal Stephenson", 2000, 3],
+    ["Luigi Mario", 1200, 1],
+    ["Ellie Arroway", 1700, 2],
+    ["Kevin Flynn", 1100, 2],
+    ["Severus Snape", 5, 1]
+]
+
+
+def home_menu():
+    """user chooses between sending thank you, creating report,
+       or exiting the program
+    """
+    print("Welcome to Mailroom Madness")
+    print("")
+    print("Choose from the following:")
+    print("")
+    print("T - Send a (T)hank You")
+    print("R - Create a (R)eport")
+    print("quit - Quit the program")
