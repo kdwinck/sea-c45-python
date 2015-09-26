b857d9cff0480b8b0436cccb43426a37e44aadf6
Kyle Winckler <kyle.winckler@gmail.com>
2015-09-25 23:53:07 -0700

    continue working on thank_you and donation functions

--- hw/hw11/mailroom.py
+++ hw/hw11/mailroom.py
@@ -31,14 +31,59 @@ def home_menu():
     user_choice = user_choice.upper()  # convert input to upper case
 
     if user_choice == "T":
-        pass
+        send_thank_you()
     elif user_choice == "R":
-        pass
+        create_report(donor_list)
     elif user_choice == "QUIT":
         pass
     else:
-        print("Invalid input.")
+        print("INVALID INPUT.")
         home_menu()
 
+
+def create_report(donor_list):
+    """Prints a list of donors, sorted by total donation amount"""
+    pass
+
+
+def send_thank_you():
+    """Prompt user for full name of donor"""
+
+    print("Please enter a name, or choose from the following: ")
+    print("")
+    print("list - Print a list of previous donors")
+    print("")
+    print("quit - Return to main menu")
+    print("")
+
+    donor_name = input("> ")
+    str_donor = str(donor_name)
+
+    if str_donor == "list":
+        print(donor_list)
+        print("")
+        send_thank_you()
+    elif str_donor == "quit":
+        home_menu()
+    else:
+        for donor in donor_list:
+            if donor[0] == str_donor:
+                donation = get_donation()
+                donor[1] += donation
+                donor[2] += 1
+                break
+
+        donation = get_donation()
+        donor_list.append([str_donor, donation, 1])
+    print(donor_list)
+
+
+def get_donation():
+    print("")
+    print("Please enter a donation amount or 'quit':")
+    print("")
+
+    donation_amount = int(input("> "))
+    return(donation_amount)
 
 home_menu()
