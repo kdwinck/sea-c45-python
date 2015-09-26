afb8200b13e352ff04c3c699aac59b9b617ec203
Kyle Winckler <kyle.winckler@gmail.com>
2015-09-26 15:27:20 -0700

    added functions and woked on program flow

--- hw/hw11/mailroom.py
+++ hw/hw11/mailroom.py
@@ -1,12 +1,3 @@
-donor_list = [
-    ["Neal Stephenson", 2000, 3],
-    ["Luigi Mario", 1200, 1],
-    ["Ellie Arroway", 1700, 2],
-    ["Kevin Flynn", 1100, 2],
-    ["Severus Snape", 5, 1]
-]
-
-
 def introduction():
     """ Print out the introduction for the user"""
 
@@ -35,10 +26,12 @@ def home_menu():
     elif user_choice == "R":
         create_report(donor_list)
     elif user_choice == "QUIT":
-        pass
+        not_done = False
+        return not_done
     else:
         print("INVALID INPUT.")
-        home_menu()
+        print("")
+        return home_menu()
 
 
 def create_report(donor_list):
@@ -49,6 +42,7 @@ def create_report(donor_list):
 def send_thank_you():
     """Prompt user for full name of donor"""
 
+    print("")
     print("Please enter a name, or choose from the following: ")
     print("")
     print("list - Print a list of previous donors")
@@ -60,11 +54,12 @@ def send_thank_you():
     str_donor = str(donor_name)
 
     if str_donor == "list":
-        print(donor_list)
         print("")
-        send_thank_you()
+        for donor in donor_list:
+            print(donor[0])
+        return send_thank_you()
     elif str_donor == "quit":
-        home_menu()
+        return home_menu()
     else:
         for donor in donor_list:
             if donor[0] == str_donor:
@@ -72,18 +67,73 @@ def send_thank_you():
                 donor[1] += donation
                 donor[2] += 1
                 break
+            else:
+                donation = get_donation()
+                donor_list.append([str_donor, donation, 1])
+                break
 
-        donation = get_donation()
-        donor_list.append([str_donor, donation, 1])
-    print(donor_list)
+    create_letter(str_donor, donation)
 
 
 def get_donation():
+    """ Prompt user for amount of donation """
+
     print("")
     print("Please enter a donation amount or 'quit':")
     print("")
 
-    donation_amount = int(input("> "))
-    return(donation_amount)
+    donation = (input("> "))
 
-home_menu()
+    donation = is_float(donation)
+    donation = str('%.2f' % donation)
+    return(donation)
+
+
+def is_float(donation):
+    """ Evaluates donation to see if it is convertable to a float
+
+    return value if it is
+    run get_donation if it is not
+
+    """
+
+    try:
+        val = float(donation)
+        return val
+    except ValueError:
+        print("INVALID INPUT!")
+        return get_donation()
+
+
+def create_letter(str_donor, donation):
+    print("")
+    print("Dear %s," % str_donor)
+    print("")
+    print("Thank you for your donation of $%s. We here at the Foundation for "
+          "Homeless Whales greatly appreciate it. Your money will go towards "
+          "creating new oceans for whales to live in." % donation)
+    print("")
+    print("Thanks again,")
+    print("")
+    print("Kyle Winckler")
+    print("")
+    print("Director, F.H.W")
+    print("")
+    print("Press Enter to Continue...")
+    print("")
+
+
+if __name__ == '__main__':
+
+    not_done = True
+
+    donor_list = [
+        ["Neal Stephenson", 2000, 3],
+        ["Luigi Mario", 1200, 1],
+        ["Ellie Arroway", 1700, 2],
+        ["Kevin Flynn", 1100, 2],
+        ["Severus Snape", 5, 1]
+    ]
+
+    while not_done:
+        not_done = home_menu()
