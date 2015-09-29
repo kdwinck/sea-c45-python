9e211bc42e2c19ad24eb177eb7c6f1ac1dcee8f6
Kyle Winckler <kyle.winckler@gmail.com>
2015-09-28 18:45:51 -0700

    fixed errors to provide correct output

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -1,6 +1,6 @@
-import string
+import random
 
-file = open("sherlock_small.txt", "r")
+file = open("sherlock.txt", "r")
 
 lines = file.readlines()
 words = []
@@ -9,24 +9,32 @@ trigrams = {}
 for line in lines:
     words += line.split(' ')
 
-remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
-words = [s.translate(remove_punctuation_map) for s in words]
+# remove newline characters from
 words = list(map(str.strip, words))
 
-for i, word in enumerate(words):
-    if (i < len(words) - 2):
-        trigrams[(word, words[i + 1])] = words[i + 2]
+# for i, word in enumerate(words):
+for i in range(len(words) - 2):
+    curr_word = words[i] + " " + words[i + 1]
+    if curr_word in trigrams:
+        trigrams[curr_word].append(words[i + 2])
+    else:
+        trigrams[curr_word] = [words[i + 2]]
 
-# print(str(words))
-# print(str(trigrams))
+word1 = 'Sherlock'
+word2 = 'Holmes'
+new_book = [word1, word2]
 
-new_book = ['He', 'had']
+done = False
 
-i = 0
-while i < 100:
-    current_words = [new_book[len(new_book) - 2], new_book[len(new_book) - 1]]
-    if tuple(current_words) in trigrams:
-        new_book.append(trigrams[tuple(current_words)])
-    i += 1
+while not done:
+    current_words = word1 + ' ' + word2
+    if current_words in trigrams:
+        random_value = random.choice(trigrams[current_words])
+        new_book.append(random_value)
+        word1 = word2
+        word2 = random_value
+    else:
+        done = True
 
-print(new_book)
+new_string = " ".join(new_book)
+print(new_string)
