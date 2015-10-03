9ff1897f5817fd84ea3eb6f861264ade56549699
Kyle Winckler <kyle.winckler@gmail.com>
2015-10-02 20:59:48 -0700

    create basic class methods and attributes

--- hw/hw18/html_render.py
+++ hw/hw18/html_render.py
@@ -7,5 +7,18 @@ Python class example.
 
 # The start of it all:
 # Fill it all in here.
+
+
 class Element(object):
-    pass
+
+    def __init__(self, tag="html", content=None):
+        self.tag = tag
+        self.content = content
+
+    def append(self, string):
+        self.content.append(string)
+
+    def render(self, file_out, ind=""):
+
+        file_out.write("<{tag}>" + '\n').format(tag=self.tag)
+        file_out.write("</{tag}" + '\n').format(tag=self.tag)
