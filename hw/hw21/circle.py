6c3f28e1a5261f8b0a06f1aeb37b9ee387f208db
Kyle Winckler <kyle.winckler@gmail.com>
2015-10-05 20:08:51 -0700

    remove 9 errors

--- hw/hw21/circle.py
+++ hw/hw21/circle.py
@@ -7,4 +7,11 @@ import math
 
 
 class Circle(object):
-    pass
+
+    def __init__(self, radius):
+        """
+        Initialize a circle with a given radius.
+        """
+        self.radius = radius
+        self.diameter = (radius * 2)
+        self.area = (math.pi * (radius ** 2))
