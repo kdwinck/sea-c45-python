d763c34de6fde6c1b8a15418c3de8d34a7e8f1b1
Kyle Winckler <kyle.winckler@gmail.com>
2015-10-07 08:58:46 -0700

    fixed all errors in first file, start on second

--- hw/hw21/circle.py
+++ hw/hw21/circle.py
@@ -14,4 +14,29 @@ class Circle(object):
         """
         self.radius = radius
         self.diameter = (radius * 2)
-        self.area = (math.pi * (radius ** 2))
+        self._area = math.pi * (radius ** 2)
+
+    def __str__(self):
+        return "Circle with radius: %.6f" % self.radius
+
+    def __repr__(self):
+        return 'Circle(%s)' % int(self.radius)
+
+    def _get_d(self):
+        return self._diameter
+
+    def _set_d(self, value):
+        self._diameter = value
+        self.radius = (value / 2)
+        self._area = math.pi * (self.radius ** 2)
+
+    def _del_d(self):
+        del self._diameter
+
+    def _get_a(self):
+        return self._area
+
+    diameter = property(_get_d, _set_d, _del_d, doc="")
+    area = property(_get_a, doc="")
+
+    # docstring="largest distance between two points on circle")
