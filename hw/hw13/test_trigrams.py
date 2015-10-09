a6ccf67d89ea8d5138f8851f4ce12ee73cf0992d
Kyle Winckler <kyle.winckler@gmail.com>
2015-10-08 17:49:41 -0700

    create 4 assert tests

--- hw/hw13/test_trigrams.py
+++ hw/hw13/test_trigrams.py
@@ -7,10 +7,31 @@ def test_open_file():
     trigrams.open_file('small_text.txt')
 
 
-def test_read_words():
-    lines = text.readlines()
+def test_create_words():
+    text = trigrams.open_file('small_text.txt')
     words = []
-    for line in lines:
+    for line in text:
+        line = line.strip()
         words += line.split(' ')
-    words = trigrams.create_words('small_text.txt')
     assert(len(words) > 0)
+
+
+def test_create_trigrams():
+    text = trigrams.open_file('small_text.txt')
+    words = []
+    for line in text:
+        line = line.strip()
+        words += line.split(' ')
+    trigrams_dic = {}
+    for i in range(len(words) - 2):
+        curr_word = words[i] + " " + words[i + 1]
+        if curr_word in trigrams_dic:
+            trigrams_dic[curr_word].append(words[i + 2])
+        else:
+            trigrams_dic[curr_word] = [words[i + 2]]
+    assert(len(trigrams_dic) > 0)
+
+
+def test_close_file():
+    text = trigrams.open_file('small_text.txt')
+    trigrams.close_file(text)
